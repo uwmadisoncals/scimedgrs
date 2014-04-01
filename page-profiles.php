e46b75f507b084eab7e305d019fd0bc6a1bcfaeb
Al Nemec <al.nemec@gmail.com>
2014-04-01 11:51:50 -0500

    Added the new profiles visual style option

---
+++ page-profiles.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Profiles
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+				<?php $args = array( 'post_type' => 'student_profiles', 'meta_key' => 'last_name', 'posts_per_page' => 200, 'orderby' => 'last_name', 'order' => 'ASC' );
+					$loop = new WP_Query( $args );
+					while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+					<?php get_template_part( 'content', 'profiles' ); ?>
+
+					
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
